7cb2e34e7ea2c7bbdbeb5ce9c37b634355546423
ogjong <59124095+ogjong@users.noreply.github.com>
2021-06-23 20:27:05 -0500

    Learning print and input
    Simple program using the print and input function as well as variables, f-string and concatenation.

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator\n")
+city = input("What is the name of your favorite city? \n")
+pet = input("What is the name of your favorite pet? \n")
+print(f"Your band name could be {city} {pet}!\n")
+
+print("Your band name could be " + pet + " " + city)
