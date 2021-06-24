91d6a51f2ec1bb7e19ef3098518ccdf3e6ef1bb0
Jon G <59124095+ogjong@users.noreply.github.com>
2021-06-23 22:48:44 -0500

    Update band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -3,4 +3,4 @@ city = input("What is the name of your favorite city? \n")
 pet = input("What is the name of your favorite pet? \n")
 print(f"Your band name could be {city} {pet}!\n")
 
-print("Your band name could be " + pet + " " + city)
+#print("Your band name could be " + pet + " " + city)
