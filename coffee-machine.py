b70a474ce074650bbd965d7c52065fc0c8af2cd3
Jon G <59124095+ogjong@users.noreply.github.com>
2021-08-30 22:37:05 -0500

    Coffee Machine Program
    The most challenging program to date. The machine should prompt the user for their order. 
    If you type "off", it will turn the machine off.
    If you type report, it will give you an inventory or the milk, water, coffee and profit.
    When you type the drink you want, it will check the ingredients against the inventory.
    If there is enough, it will make your drink, give change if necessary (or refund you if you don't give enough money), and add the profit to the report.
    If there isn't enough inventory, it will ask you to pick another drink.

--- coffee-machine.py
+++ coffee-machine.py
@@ -1,51 +1,70 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from resources import MENU, ingredients, coins
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
 
-coins = {
-    "quarters": .25,
-    "dimes": .10,
-    "nickels": .05,
-    "pennies": .01,
-}
 def make_payment():
-    amount_paid = 0
+    payment = 0
+    print("Please insert coins.")
     for item in coins:
         coin_value = (coins[item])
         coin_amount = int(input(f"How many {item}? "))
         total_coin_value = coin_value * coin_amount
-        amount_paid += total_coin_value
+        payment += total_coin_value
+    return payment
 
-    return amount_paid
+
+def cost(coffee_type):
+    selection_cost = MENU[coffee_type]["cost"]
+    return selection_cost
 
 
-paid = make_payment()
-print(paid)
+def check_ingredients(coffee_type):
+    chosen_drink_ingredients = MENU[coffee_type]["ingredients"]
+    available_machine_ingredients = ingredients
+    for item in available_machine_ingredients:
+        if item in chosen_drink_ingredients:
+            machine_ingredients = (available_machine_ingredients[item])
+            recipe_requirements = (chosen_drink_ingredients[item])
+            if machine_ingredients < recipe_requirements:
+                print(f"Sorry, there is not enough {item}")
+                return "out"
+
+            else:
+                machine_amount_left = machine_ingredients - recipe_requirements
+                available_machine_ingredients[item] = machine_amount_left
+    return ingredients
+
+
+def run_report():
+    print(f"Water: {ingredients['water']}ml")
+    print(f"Milk: {ingredients['milk']}ml")
+    print(f"Coffee: {ingredients['coffee']}g")
+    print(f"Money: ${profit}")
+
+
+profit = 0
+run_machine = True
+while run_machine:
+    type_of_coffee = input("What would you like? (espresso/latte/cappuccino)\n").lower()
+
+    if type_of_coffee == "off":
+        run_machine = False
+
+    elif type_of_coffee == "report":
+        print(run_report())
+
+    else:
+        any_left = check_ingredients(type_of_coffee)
+        if any_left == "out":
+            run_machine = True
+        else:
+            coffee_cost = (cost(type_of_coffee))
+            profit += coffee_cost
+            amount_paid = make_payment()
+            change = amount_paid - coffee_cost
+            if amount_paid < coffee_cost:
+                print("Sorry, that's not enough money. Money refunded")
+            elif amount_paid == coffee_cost:
+                print(f"Here is your {type_of_coffee}, enjoy!")
+            else:
+                print(f"Your change is ${round(change, 2)}")
+                print(f"Here is your {type_of_coffee}, enjoy!")
